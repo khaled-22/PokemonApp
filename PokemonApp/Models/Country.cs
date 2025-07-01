117b339349461c0813d495c466b7523d7369b856
Khale <Khale@DESKTOP-IKTVM3I>
2025-07-01 20:04:47 +0200

    Add project files.

---
+++ PokemonApp/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Betyr at en person kan være fra mange land
+        public ICollection<Owner> Owners { get; set; }
+
+    }
+}
